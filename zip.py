6463630d7edc2f53c496fa7650cd4a25d98fbc3f
avargas05 <avargas05.github@outlook.com>
2022-02-19 15:14:00 -0500

    Update comments and improve variable names

--- zip.py
+++ zip.py
@@ -23,10 +23,10 @@ class ProgressBar():
         self._window.add(self._progress)
         self._thread = threading.Thread(target=self._target)
 
-    def _update_progress(self, i):
+    def _update_progress(self, status):
         """Updates the progress bar."""
         self._progress.pulse()
-        self._progress.set_text(str(i))
+        self._progress.set_text(str(status))
 
         return False
 
@@ -34,8 +34,11 @@ class ProgressBar():
         """Target for progress bar."""
         total = len(self._files)
         for i, file in enumerate(self._files):
+            # Form full path name
             name = file.get_name()
             file_name = os.path.join(self._path, name)
+
+            # Status description
             percentage = int(i / total * 100)
             progress = f'Extracting {name}   {percentage}%   ({i}/{total})'
 
@@ -82,52 +85,65 @@ class UnzipMenuProvider(GObject.GObject, Caja.MenuProvider):
     """Context menu for commands."""
     def _extract_here(self, menu, files):
         """Extracts selected files to current location."""
-        # Change to correct directory
+        # Change to current directory to extract to
         path = os.path.dirname(files[0].get_location().get_path())
         os.chdir(path)
+
+        # Commence extraction and progress bar
         progress = ProgressBar(path, files)
         progress.start()
         Gtk.main()
 
     def _extract_to(self, menu, files):
-        """Extracts selected files to chose location."""
+        """Extracts selected files to chosen location."""
+        # Open dialog box for choosing a folder
         directory = select_folder()
         if directory:
+            # Get current directory forming full path name
             path = os.path.dirname(files[0].get_location().get_path())
             os.chdir(path)
+
+            # Commence extraction and progress
             progress = ProgressBar(path, files, directory)
             progress.start()
             Gtk.main()
 
     def get_file_items(self, window, files):
         """Gets files selected and connects functions."""
+        # Important to get copy since selecting a file during extraction
+        # changes list of files and locks up the instance of Caja
         all_files = files.copy()
 
         # Set up the top menu with a submenu
         top_menu_item = Caja.MenuItem(
                 name='UnzipMenuProvider::Unzip',
-                label='Unzip',
+                label='Zip Menu',
                 tip='',
                 icon='')
 
         submenu = Caja.Menu()
         top_menu_item.set_submenu(submenu)
 
-        # Setup the extra here command
-        extract_here = Caja.MenuItem(
+        # Setup the extract here command
+        extract_here_menu_item = Caja.MenuItem(
                 name='UnzipMenuProvider::Extract Here',
                 label='Extract Here',
                 tip='',
                 icon='')
 
-        submenu.append_item(extract_here)
-        extract_here.connect('activate', self._extract_here, all_files)
+        submenu.append_item(extract_here_menu_item)
+        extract_here_menu_item.connect(
+                'activate',
+                self._extract_here,
+                all_files)
 
+        # Setup the extract to command
         extract_to_menu_item = Caja.MenuItem(
                 name='UnzipMenuProvider::Extract To',
                 label='Extract To',
                 tip='',
                 icon='')
+
         submenu.append_item(extract_to_menu_item)
         extract_to_menu_item.connect('activate', self._extract_to, all_files)
 
